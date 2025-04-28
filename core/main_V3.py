45c59bfe08270afb014d26a28c19e3d2a336ff7e
Youssef Elzedy <youssefelzedy90@gmail.com>
2025-04-28 22:36:56 +0300

    Refactor main_V3.py to integrate new OCR model and enhance character segmentation; update detect_text_yolo function signature

--- core/main_V3.py
+++ core/main_V3.py
@@ -1,29 +1,33 @@
 from ultralytics import YOLO
+from tensorflow import keras
+
 import cv2
-
-from sort.sort import *
-from utils.util_V2 import get_car, detect_text_yolo, separate_numbers_letters, format_license
-import torch
-
 import numpy as np
 import pandas as pd
+from sort.sort import *
+import torch
+
+from utils.util_V2 import get_car, detect_text_yolo, separate_numbers_letters, format_license
+from utils.detectCharacters import predict_characters
+from utils.detectCharacters import segment_characters
 
 
 results = {}
 
 mot_tracker = Sort()
 
-
 # load models
 coco_model = YOLO('./models/yolov8n.pt')
-ocr_yolo_model = 'models/yolo11m_car_plate_ocr.pt'
+ocr_yolo_model = YOLO('models/yolo11m_car_plate_ocr.pt')
 license_plate_detector = YOLO('./models/license_plate_detector.pt')
 license_plate_detector2 = YOLO('./models/yolo11m_car_plate_trained.pt')
+new_OCR_model = keras.models.load_model('model.h5')
 
 # # load video
 # cap = cv2.VideoCapture('./Input-videos/test3.mp4')
 
 vehicles = [2, 3, 5, 7]
+
 
 def final_model(frame):
     # detect vehicles
@@ -52,32 +56,36 @@ def final_model(frame):
             # crop license plate
             license_plate_crop = frame[int(
                 y1):int(y2), int(x1): int(x2), :]
-            license_plate_crop = cv2.cvtColor(license_plate_crop, cv2.COLOR_BGR2HSV)
+            license_plate_crop = cv2.cvtColor(
+                license_plate_crop, cv2.COLOR_BGR2HSV)
             h, s, v = cv2.split(license_plate_crop)
-            s = cv2.add(s, 60) 
+            s = cv2.add(s, 60)
             hsv_enhanced = cv2.merge([h, s, v])
             image_enhanced = cv2.cvtColor(hsv_enhanced, cv2.COLOR_HSV2BGR)
 
             recognized_image, detected_numbers, detected_letters, character_bboxes, all_predictions = detect_text_yolo(
                 ocr_yolo_model, image_enhanced
             )
+            
+            # get character bboxes
+            segment_characters(license_plate_crop, character_bboxes, new_OCR_model)
 
             # print("Image Text ==========================")
             # print(detected_numbers, detected_letters)
             # print("=====================================")
-            print("All Predictions ======================")
-            print(all_predictions)
-            print("======================================")
+            # print("All Predictions ======================")
+            # print(all_predictions)
+            # print("======================================")
 
-            sorted_data = sorted(all_predictions, key=lambda x: x[0])
-            
-            # split numbers and letters
-            print(x2 - x1)
-            num, char = separate_numbers_letters(sorted_data, x2 - x1)
-            
-            # format license plate
-            license_plate_text = format_license(num, char)
-            print("License Plate =======================")
-            print(license_plate_text)
-            print("=====================================")
-    return license_plate_text
+            # sorted_data = sorted(all_predictions, key=lambda x: x[0])
+
+            # # split numbers and letters
+            # print(x2 - x1)
+            # num, char = separate_numbers_letters(sorted_data, x2 - x1)
+
+            # # format license plate
+            # license_plate_text = format_license(num, char)
+            # print("License Plate =======================")
+            # print(license_plate_text)
+            # print("=====================================")
+    return 0
--- utils/util_V2.py
+++ utils/util_V2.py
@@ -23,68 +23,6 @@ dict_int_to_char = {'1': 'alif',
                     '6': 'meem'}
 
 
-# def write_csv(results, output_path):
-#     """
-#     Write the results to a CSV file.
-
-#     Args:
-#         results (dict): Dictionary containing the results.
-#         output_path (str): Path to the output CSV file.
-#     """
-#     with open(output_path, 'w') as f:
-#         f.write('{},{},{},{},{},{},{}\n'.format('frame_nmr', 'car_id', 'car_bbox',
-#                                                 'license_plate_bbox', 'license_plate_bbox_score', 'license_number',
-#                                                 'license_number_score'))
-
-#         for frame_nmr in results.keys():
-#             for car_id in results[frame_nmr].keys():
-#                 print(results[frame_nmr][car_id])
-#                 if 'car' in results[frame_nmr][car_id].keys() and
-#                 'license_plate' in results[frame_nmr][car_id].keys() and
-#                 'text' in results[frame_nmr][car_id]['license_plate'].keys():
-#                     f.write('{},{},{},{},{},{},{}\n'.format(frame_nmr,
-#                                                             car_id,
-#                                                             '[{} {} {} {}]'.format(
-#                                                                 results[frame_nmr][car_id]['car']['bbox'][0],
-#                                                                 results[frame_nmr][car_id]['car']['bbox'][1],
-#                                                                 results[frame_nmr][car_id]['car']['bbox'][2],
-#                                                                 results[frame_nmr][car_id]['car']['bbox'][3]),
-#                                                             '[{} {} {} {}]'.format(
-#                                                                 results[frame_nmr][car_id]['license_plate']['bbox'][0],
-#                                                                 results[frame_nmr][car_id]['license_plate']['bbox'][1],
-#                                                                 results[frame_nmr][car_id]['license_plate']['bbox'][2],
-#                                                                 results[frame_nmr][car_id]['license_plate']['bbox'][3]),
-#                                                             results[frame_nmr][car_id]['license_plate']['bbox_score'],
-#                                                             results[frame_nmr][car_id]['license_plate']['text'],
-#                                                             results[frame_nmr][car_id]['license_plate']['text_score'])
-#                             )
-#         f.close()
-
-
-# def license_complies_format(text):
-#     """
-#     Check if the license plate text complies with the required format.
-
-#     Args:
-#         text (str): License plate text.
-
-#     Returns:
-#         bool: True if the license plate complies with the format, False otherwise.
-#     """
-#     if len(text) != 7:
-#         return False
-
-#     if (text[0] in string.ascii_uppercase or text[0] in dict_int_to_char.keys()) and
-#     (text[1] in string.ascii_uppercase or text[1] in dict_int_to_char.keys()) and
-#     (text[2] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] or text[2] in dict_char_to_int.keys()) and
-#     (text[3] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] or text[3] in dict_char_to_int.keys()) and
-#     (text[4] in string.ascii_uppercase or text[4] in dict_int_to_char.keys()) and
-#     (text[5] in string.ascii_uppercase or text[5] in dict_int_to_char.keys()) and
-#     (text[6] in string.ascii_uppercase or text[6] in dict_int_to_char.keys()):
-#         return True
-#     else:
-#         return False
-
 def separate_numbers_letters(predictions, width):
     """
     Separate the numbers and letters in the license plate text.
@@ -153,9 +91,20 @@ def format_license(num, char):
     return license_plate_
 
 
-def detect_text_yolo(ocr_yolo_model, cropped_image):
-    # Load the YOLO model
-    model = YOLO(ocr_yolo_model)
+def detect_text_yolo(model, cropped_image):
+
+    """
+    Detect text in the cropped image using YOLOv8.
+    Args:
+        model (YOLO): YOLOv8 model.
+        cropped_image (numpy.ndarray): Cropped image of the license plate.
+    Returns:
+        recognized_image (numpy.ndarray): Image with detected text.
+        detected_numbers (list): List of detected numbers.
+        detected_letters (list): List of detected letters.
+        character_bboxes (list): List of bounding boxes for each character.
+        all_predictions (list): List of all predictions with bounding boxes and text.
+    """
 
     # Run prediction
     results = model.predict(source=cropped_image, conf=0.25)

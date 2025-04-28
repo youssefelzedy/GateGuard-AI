b33a179d9162a1bcd35b688e2ec8ea042260a592
Youssef Elzedy <youssefelzedy90@gmail.com>
2025-04-28 22:03:29 +0300

    Add character detection module with prediction functionality

---
+++ utils/detectCharacter.py
@@ -0,0 +1,50 @@
+import torch
+import string
+import easyocr
+from ultralytics import YOLO
+import cv2
+import numpy as np
+import pandas as pd
+from PIL import Image
+
+code = {'س': 0, 'و': 1, 'ط': 2, 'ف': 3, 'ا': 4, '٣': 5, '٩': 6, 'ق': 7, '١': 8, '٤': 9, 'ص': 10, 'ب': 11, '٥': 12,
+        'ي': 13, 'ج': 14, '٧': 15, '٨': 16, 'ه': 17, 'د': 18, '٢': 19, 'م': 20, 'ر': 21, 'ل': 22, 'ن': 23, '٦': 24, 'ع': 25}
+
+
+def getname(n):
+    """
+    Get the name of the character from its code.
+    This function takes a character code as input and returns the corresponding character name.
+    The mapping is defined in the 'code' dictionary.
+
+    Args:
+
+          n (int): The character code.
+
+          Returns:
+          str: The name of the character.
+   """
+    for k, v in code.items():
+        if v == n:
+            return k
+
+
+def predict_character(model_n, img_1):
+    """Predict the character in the image using the trained model.
+    This function takes the image and a trained model as input, preprocesses the image,
+    and predicts the character using the model. The predicted character is then displayed.
+    Args:
+        model_n (torch.nn.Module): The trained model for character recognition.
+        path (str): The path to the image file.
+
+        Returns:
+        None
+    """
+
+    img_1 = cv2.resize(img_1, (32, 32))
+    img_1 = np.array(img_1)
+    img_1_3d = img_1.reshape((1, 32, 32))
+    img_1_prob = model_n.predict(np.array(img_1_3d))
+    img_1_pred = img_1_prob.argmax(axis=1)
+    character = getname(img_1_pred)
+    return character
